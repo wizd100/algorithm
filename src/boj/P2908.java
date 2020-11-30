23aef8d8abf8aa64d4326faf7f40b89b3a692742
bkh-mac <wizd100@gmail.com>
2020-11-30 18:58:28 +0900

    백준 2908번 복습 겸 다른 방법으로 품

--- src/boj/P2908.java
+++ src/boj/P2908.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class P2908 {
     public static void main(String[] args) {
+        //solution1();
+        solution2();
+    }
+
+    public static void solution1() {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
@@ -23,6 +28,23 @@ public class P2908 {
         } else {
             System.out.println(reverseB);
         }
+    }
 
+    public static void solution2() {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        int reverse1 = Integer.parseInt(sb1.append(n1).reverse().toString());
+        int reverse2 = Integer.parseInt(sb2.append(n2).reverse().toString());
+
+        if (reverse1 > reverse2) {
+            System.out.println(reverse1);
+        } else {
+            System.out.println(reverse2);
+        }
     }
 }
