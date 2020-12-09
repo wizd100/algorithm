c2629789e2125fd451e04a3a9dca6d178cd7eb08
bkh-mac <wizd100@gmail.com>
2020-12-09 17:20:00 +0900

    백준 2292 복습

--- src/boj/P2292.java
+++ src/boj/P2292.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class P2292 {
     public static void main(String[] args) {
+        //solution1();
+        solution2();
+    }
+
+    public static void solution1() {
         Scanner sc = new Scanner(System.in);
 
         int start = 2;
@@ -22,5 +27,31 @@ public class P2292 {
         }
 
         System.out.println(count);
+    }
+
+    public static void solution2() {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int start = 2;
+        int plus = 6;
+        int end = start + plus - 1;
+        int count = 2;
+
+        if (N == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        while (true) {
+            System.out.printf("%d ~ %d\n", start, end);
+            if (N >= start && N <= end) {
+                System.out.println(count);
+                break;
+            }
+            start = start + plus;
+            plus += 6;
+            end = start + plus - 1;
+            count++;
+        }
     }
 }
