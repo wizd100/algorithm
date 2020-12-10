d237d2a73ccddc4f5c7f43d8b436da572a10dc5a
bkh-mac <wizd100@gmail.com>
2020-12-10 15:30:52 +0900

    백준 1193 문제 복습

--- src/boj/P1193.java
+++ src/boj/P1193.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class P1193 {
     public static void main(String[] args) {
+        //solution1();
+        solution2();
+    }
+
+    public static void solution1() {
         Scanner sc = new Scanner(System.in);
 
         int x = sc.nextInt();
@@ -55,5 +60,56 @@ public class P1193 {
         }
 
         System.out.printf("R : %d/%d : %d\n", i, j, count);
+    }
+
+    public static void solution2() {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int a = 1;
+        int b = 2;
+        int count = 2;
+        boolean up = false;
+        boolean down = true;
+
+        if (X == 1) {
+            System.out.println("1/1");
+            return;
+        }
+
+        while (X != count) {
+            //System.out.printf("%d : %d/%d\n", count, a, b);
+            //좌측 하단으로 이동
+            if (down) {
+                //맨 좌측 도달했을때 밑으로 이동
+                if (b == 1) {
+                    a++;
+                    up = true;
+                    down = false;
+                    count++;
+                    continue;
+                }
+                a++;
+                b--;
+                count++;
+                continue;
+            }
+
+            if (up) {
+                //맨위로 도달했을때 오른쪽으로 이동
+                if (a == 1) {
+                    b++;
+                    count++;
+                    up = false;
+                    down = true;
+                    continue;
+                }
+                a--;
+                b++;
+                count++;
+                continue;
+            }
+        }
+
+        System.out.printf("%d/%d", a, b);
     }
 }
