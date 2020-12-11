c4709018c9e12092281820de1449a9d174bc2c51
bkh-mac <wizd100@gmail.com>
2020-12-11 17:57:08 +0900

    백준 10250번 문제

--- src/boj/P10250.java
+++ src/boj/P10250.java
@@ -4,19 +4,8 @@ import java.util.Scanner;
 
 public class P10250 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int H;
-        int W;
-        int N;
-
-        for (int i = 0; i < T; i++) {
-            H = sc.nextInt();
-            W = sc.nextInt();
-            N = sc.nextInt();
-
-            System.out.println(searchRoom(H, W, N));
-        }
+        //solution1();
+        solution2();
     }
 
     public static int searchRoom(int H, int W, int N) {
@@ -32,5 +21,47 @@ public class P10250 {
         }
 
         return 0;
+    }
+
+    public static void solution1() {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int H;
+        int W;
+        int N;
+
+        for (int i = 0; i < T; i++) {
+            H = sc.nextInt();
+            W = sc.nextInt();
+            N = sc.nextInt();
+
+            System.out.println(searchRoom(H, W, N));
+        }
+    }
+
+    public static void solution2() {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
+
+            int roomNumber = 101;
+
+            for (int j = 0; j < N; j++) {
+                //System.out.printf("%d\n", roomNumber);
+
+                if (roomNumber / (H * 100) == 1) {
+                    roomNumber = (roomNumber % 100) + 101;
+                    continue;
+                }
+
+                roomNumber += 100;
+            }
+
+            System.out.println(roomNumber);
+        }
     }
 }
