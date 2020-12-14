7405af199a61d95d0e56620e956bf042118d847e
bkh <wizd100@gmail.com>
2020-12-14 23:51:00 +0900

    백준 1002번 문제

---
+++ src/boj/P1002.java
@@ -0,0 +1,32 @@
+package boj;
+
+import java.util.Scanner;
+
+public class P1002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int r1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            int r2 = sc.nextInt();
+
+            //무한대
+            if (x1 == x2 && y1 == y2 && r1 == r2) {
+                System.out.println(-1);
+                continue;
+            }
+
+            //0
+            
+
+            //1
+
+            //2
+        }
+    }
+}
