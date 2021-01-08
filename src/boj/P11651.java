9829f38ffef9e3b1aad0743a15ad08c929b82a70
bkh-mac <wizd100@gmail.com>
2021-01-08 23:31:05 +0900

    백준 11651번

--- src/boj/P11651.java
+++ src/boj/P11651.java
@@ -1,5 +1,7 @@
 package boj;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class P11651 {
@@ -11,6 +13,17 @@ public class P11651 {
         for (int i = 0; i < N; i++) {
             arr[i][0] = sc.nextInt();
             arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.printf("%d %d\n", arr[i][0], arr[i][1]);;
         }
     }
 }
