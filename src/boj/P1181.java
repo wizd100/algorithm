fef0581c7fd91614da3a389fb55ffd2eb06b8668
bkh-mac <wizd100@gmail.com>
2021-01-12 14:44:20 +0900

    백준 1181번 문제

--- src/boj/P1181.java
+++ src/boj/P1181.java
@@ -8,8 +8,9 @@ public class P1181 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        sc.nextLine();
+        sc.nextLine(); //라인 개행 처리
         String[] arr = new String[N];
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextLine();
@@ -18,16 +19,27 @@ public class P1181 {
         Arrays.sort(arr, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if (o1.length() < o2.length()) {
-                    return o1.length() - o2.length();
+                //단어 길이가 같은 경우 사전 순으로 정렬
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
                 }
 
-                return o2.length() - o1.length();
+                //단어 길이로 정렬
+                return o1.length() - o2.length();
             }
         });
 
-        for (int i = 0; i < N; i++) {
-            System.out.println(arr[i]);
+        sb.append(arr[0] + "\n");
+
+        for (int i = 1; i < N; i++) {
+            //중복 단어 제거
+            if (arr[i].equals(arr[i - 1])) {
+                continue;
+            }
+
+            sb.append(arr[i] + "\n");
         }
+
+        System.out.println(sb);
     }
 }
