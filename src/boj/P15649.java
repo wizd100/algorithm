fb9177c4adeab9c14b72d9f91e2eb79a8a2a724a
bkh-mac <wizd100@gmail.com>
2021-01-13 23:32:26 +0900

    백준 15649번 문제

---
+++ src/boj/P15649.java
@@ -0,0 +1,15 @@
+package boj;
+
+import java.util.Scanner;
+
+public class P15649 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        while (true) {
+            
+        }
+    }
+}
