61bd39dcaa2a888f18b0c35d6a678017da9e8202
bkh <wizd100@gmail.com>
2021-01-18 23:49:35 +0900

    백준 11653

---
+++ src/boj/P11653.java
@@ -0,0 +1,12 @@
+package boj;
+
+import java.util.Scanner;
+
+public class P11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println(N);
+    }
+}
